01d9832e48d402677cb7b390074ec178caecd40c
Edgar <edgarcancinoe@gmail.com>
2026-01-20 15:30:25 +0000

    cuda exposed

--- run_server.sh
+++ run_server.sh
@@ -5,11 +5,14 @@
 source /home/jose/conda/etc/profile.d/conda.sh
 conda activate vla
 
-# Set HuggingFace cache directories to avoid permission issues with /opt/cache
-export HF_HOME="$HOME/.cache/huggingface"
-export TRANSFORMERS_CACHE="$HOME/.cache/huggingface/transformers"
-export HF_DATASETS_CACHE="$HOME/.cache/huggingface/datasets"
+# Use GPU 7 (the free one)
+export CUDA_VISIBLE_DEVICES=7
+
+# Set HuggingFace cache directories to a location with 1.7TB free space
+export HF_HOME="$HOME/huggingface_cache"
+export TRANSFORMERS_CACHE="$HOME/huggingface_cache/transformers"
+export HF_DATASETS_CACHE="$HOME/huggingface_cache/datasets"
+export HF_HUB_CACHE="$HOME/huggingface_cache/hub"
 
 # Run the policy server
 python -m lerobot.async_inference.policy_server --config_path launch_server.yaml "$@"
-
