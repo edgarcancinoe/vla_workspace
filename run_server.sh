ae309c895b4b97a7bfcb7ebbd31b2ad9ad330f08
Edgar <edgarcancinoe@gmail.com>
2026-01-20 15:20:31 +0000

    righut path for env

--- run_server.sh
+++ run_server.sh
@@ -2,8 +2,8 @@
 # Script to run the lerobot policy server with the thesis conda environment
 
 # Activate conda environment
-source /opt/anaconda3/etc/profile.d/conda.sh
-conda activate thesis
+source /home/jose/conda/etc/profile.d/conda.sh
+conda activate vla
 
 # Run the policy server
 python -m lerobot.async_inference.policy_server --config_path launch_server.yaml "$@"
