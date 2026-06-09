5d5ab5ff040ede3a1b20bb5f66d41a5bbc5686c7
“Edgar” <edgarcancinoe@gmail.com>
2026-06-10 00:07:37 +0200

    hangnin

--- src/thesis_vla/__init__.py
+++ src/thesis_vla/__init__.py
@@ -1,5 +1,1 @@
 """Thesis VLA workspace package."""
-
-from thesis_vla.policies import ensure_thesis_policy_registration as _ensure_thesis_policy_registration
-
-_ensure_thesis_policy_registration()
